15427ebe884fe1b618a5fb77f79a1a08592c5815
umutcolak <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>
2022-08-29 22:01:33 +0300

    Support GitHub Actions for Jenkins Job Trigger

--- entrypoint.py
+++ entrypoint.py
@@ -18,12 +18,21 @@ JENKINS_URL = mandatory_arg(sys.argv[1])
 JENKINS_TOKEN = mandatory_arg(sys.argv[2])
 JENKINS_USER = mandatory_arg(sys.argv[3])
 JOB_PATH = mandatory_arg(sys.argv[4])
-
+JOB_PARAMS = sys.argv[5]
 # not mandatory
-JOB_PARAMS = sys.argv[5] or '{}'
+params = ""
+
+
+def is_json():
+    try:
+        params = json.loads(JOB_PARAMS)
+    except ValueError as e:
+        return JOB_PARAMS
+    return params
+
 
 # create/connect jenkins server
 server = jenkins.Jenkins(url=JENKINS_URL, username=JENKINS_USER, password=JENKINS_TOKEN)
 
 # build job
-server.build_job(JOB_PATH, parameters=json.loads(JOB_PARAMS), token=JENKINS_TOKEN)
+server.build_job(JOB_PATH, parameters=is_json(), token=JENKINS_TOKEN)
